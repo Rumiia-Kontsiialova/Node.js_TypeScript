52452e1bb9a372efc5f15508b0595cf6efd5bdce
Rumiia Kontsiialova <rumiia.kontsiyalova@gmail.com>
2025-08-25 19:36:14 +0200

    added second part of 16 homework

--- homework_1_16/src/index.js
+++ homework_1_16/src/index.js
@@ -5,4 +5,17 @@
 function greetUser(name) {
     console.log("\u041F\u0440\u0438\u0432\u0435\u0442, ".concat(name));
 }
+// const userName: string = "Rumiia"; // const не обязательна, с ней вызов функции будет: greetUser(userName);
 greetUser('Rumiia');
+;
+function printPersonInfo(person) {
+    console.log("Name: ".concat(person.name));
+    console.log("Age: ".concat(person.age));
+    console.log("City: ".concat(person.city));
+}
+var newPerson = {
+    name: 'Rumiia',
+    age: 31,
+    city: 'Magdeburg',
+};
+printPersonInfo(newPerson);
--- homework_1_16/src/index.ts
+++ homework_1_16/src/index.ts
@@ -10,3 +10,34 @@ function greetUser(name: string) {
 // const userName: string = "Rumiia"; // const не обязательна, с ней вызов функции будет: greetUser(userName);
 
 greetUser('Rumiia');
+
+// Задание 2
+// Типизация функции с объектом в качестве параметра
+// Создайте интерфейс `Person`, который описывает человека с полями `name`, `age`, и `city`.
+// Напишите функцию `printPersonInfo`, которая принимает объект типа `Person` 
+// и выводит информацию о человеке в формате: `"Имя: <name>, Возраст: <age>, Город: <city>"`.
+
+interface Person {
+    name: string;
+    age: number;
+    city: string;
+};
+
+function printPersonInfo (person: Person) : void {
+    console.log(`Name: ${person.name}`);
+    console.log(`Age: ${person.age}`);
+    console.log(`City: ${person.city}`);
+}
+
+const newPerson: Person = {
+    name: 'Rumiia',
+    age: 31,
+    city: 'Magdeburg',
+}
+
+ printPersonInfo(newPerson);
+
+// Задание 3
+// Простая типизация для числового параметра
+// Напишите функцию `squareNumber`, которая принимает число и возвращает его квадрат. 
+// Используйте строгую типизацию.
