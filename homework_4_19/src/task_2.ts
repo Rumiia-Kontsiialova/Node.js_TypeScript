e50e7aeff3750bb90131435ea1621187496ea956
Rumiia Kontsiialova <rumiia.kontsiyalova@gmail.com>
2025-09-01 17:08:56 +0200

    added third part of 19 homework

--- homework_4_19/src/task_2.ts
+++
@@ -1,8 +0,0 @@
-// Задание 2
-// Определите интерфейс `StringToBooleanFunction` для функции, которая принимает строку 
-// и возвращает `boolean` (например, проверяет, является ли строка пустой). 
-// Реализуйте такую функцию.
-
-interface StringToBooleanFunction {
-    (str: string): boolean
-}
